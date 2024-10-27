b86412c9f4f74620c5f7dc7430f15917f9226876
gracedonaher <113559499+gracedonaher@users.noreply.github.com>
2024-10-27 14:32:41 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>UD CISC275 with React Hooks and TypeScript</h1>
             </header>
             <p>
                 Grace Donaher
-                Hello World
             </p>
+            <p>Hello World</p>
         </div>
     );
 }
