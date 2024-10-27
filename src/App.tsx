1a47aca482e05d694874f88cbe0a24a77c9750a2
gracedonaher <113559499+gracedonaher@users.noreply.github.com>
2024-10-27 15:07:19 -0400

    adding two columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
+import {Button} from 'react-bootstrap';
 import "./App.css";
 
 function App(): React.JSX.Element {
     return (
-        <div className="App">
+        <div className="App" style={ {width: 2}>
             <header className="App-header">
                 <h1>UD CISC275 with React Hooks and TypeScript</h1>
             </header>
-            <p>
+            <Col>
                 Grace Donaher
-            </p>
             <p>
                 Hello World
                 <ul>
@@ -18,10 +18,13 @@ function App(): React.JSX.Element {
                     <li>Ciao</li>
                 </ul>
             </p>
+            </Col>
+            <Col
             <p>
                 <img src="computerSetup.jpeg" alt="Pink LED computer setup"/>
                 <Button onClick={ () => { console.log("Hello World") } }>Log Hello World</Button>
             </p>
+            </Col>
         </div>
     );
 }
